59869456dd6fe12a325ab7a9370f0293d6abf364
thebigguy270 <37880246+thebigguy270@users.noreply.github.com>
2023-11-07 22:24:46 -0500

    Update Jeu.java

--- Snake/src/snakepk/Jeu.java
+++ Snake/src/snakepk/Jeu.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Jeu extends JFrame {
 	public Jeu() {
 		this.add(new Graphismes());
-        this.setTitle("Snake Game");
+        this.setTitle("Serpent");
         this.pack();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
